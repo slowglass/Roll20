0868582845945b857044559c8317b0138cedcada
Chris Davies <cdavies@itrsgroup.com>
2020-04-10 21:43:20 +0100

    ThirdPartyScripts

---
+++ ThirdParty/getScripts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+curl -s https://gist.githubusercontent.com/VoltCruelerz/e271ed7004e97f8183b9f0145d29b00e/raw/04553b54a8e58ba9ab9813ca636b0c06990d3acc/AirbagStart.js > AirbagStart.js 
+curl -s https://gist.githubusercontent.com/VoltCruelerz/e271ed7004e97f8183b9f0145d29b00e/raw/04553b54a8e58ba9ab9813ca636b0c06990d3acc/AirbagStop.js > AirbagStop.js 
+
+curl -s https://raw.githubusercontent.com/Roll20/roll20-api-scripts/master/HealthColors/1.6.0/HealthColors.js > HealthColors.js 
+
+curl -s https://raw.githubusercontent.com/Roll20/roll20-api-scripts/master/5th%20Edition%20OGL%20by%20Roll20%20Companion/1.4.1/5th%20Edition%20OGL%20by%20Roll20%20Companion.js  > "5th Edition OGL by Roll20 Companion.js"
+curl -s https://raw.githubusercontent.com/RobinKuiper/Roll20APIScripts/master/Concentration/Concentration.js > Concentration.js 
+curl -s https://raw.githubusercontent.com/Roll20/roll20-api-scripts/master/TokenMod/TokenMod.js > TokenMod.js
+curl -s https://raw.githubusercontent.com/Roll20/roll20-api-scripts/master/GMSheet/GMSheet.js > GMSheet.js 
+curl -s https://raw.githubusercontent.com/Roll20/roll20-api-scripts/master/HtmlBuilder/1.0/script.js > HtmlBuilder.js
+curl -s https://raw.githubusercontent.com/Roll20/roll20-api-scripts/master/CarryTokens/2.0/carry.js > CarryTokens.js
+curl -s https://raw.githubusercontent.com/Roll20/roll20-api-scripts/master/DryErase/DryErase.js > DryErase.js 
+
