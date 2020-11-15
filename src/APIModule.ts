c7559e7db522bfed3bc0fd6ae2bd3040bbb8106b
Chris Davies <Christopher.James.Davies@gmail.com>
2020-11-15 08:48:18 +0000

    Add getState to APIModule

--- src/APIModule.ts
+++ src/APIModule.ts
@@ -2,14 +2,26 @@
 * Author: Chris Davies
 * GitHub: https://github.com/slowglass/Roll20.git
 */
-
+declare var _:any;
 
 function debug(m:string, o:any) {
     log(m+":"+JSON.stringify(o))
 }
 abstract class APIModule {
     abstract version: string
-    protected abstract initialise() : void
+
+    protected abstract initialise(): void
+
+    getState(name: string, defaults: any, override: any) {
+        if (!_.has(state, name)) {
+            state[name] = state[name] || {};
+        }
+        if (override || Object.keys(state[name]).length === 0) {
+            state[name] = JSON.parse(JSON.stringify(defaults));
+        }
+        return state[name];
+    }
+
     register() {
         log(`Register Module: ${this.constructor.name} (${this.version})`)
         this.initialise()
