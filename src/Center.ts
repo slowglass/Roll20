9f016ce54d3516ec7f8cd04c3c19b9679c003523
Chris Davies <Christopher.James.Davies@gmail.com>
2020-11-15 08:54:09 +0000

    Code tidy Center.ts

--- src/Center.ts
+++ src/Center.ts
@@ -6,6 +6,7 @@
 class Center extends APIModule {
     readonly version = "0.2"
     private pageID = ''
+
     private pingPlayers() {
         const currentPageID = Campaign().get('playerpageid');
         if (this.pageID === currentPageID) return
@@ -13,7 +14,7 @@ class Center extends APIModule {
         players.forEach((p) => {
             const pid = p.get("_id");
             const isGM = playerIsGM(pid);
-            let tokens = Roll20.filterGraphics((obj:Roll20Object) => {
+            let tokens = Roll20.filterGraphics((obj: Roll20Object) => {
                 if (obj.get("_subtype") !== "token") return false;
                 if (obj.get("_pageid") !== currentPageID) return false;
                 const character = getObj("character", obj.get("represents"));
@@ -23,15 +24,21 @@ class Center extends APIModule {
                 return isGM || controlledby.split(',').includes(pid);
             });
             if (tokens.length === 0)
-                tokens = Roll20.findGraphics({ _subtype:"token", _pageid: currentPageID, name: 'Party' });
+                tokens = Roll20.findGraphics({_subtype: "token", _pageid: currentPageID, name: 'Party'});
             const token = tokens.shift()
             if (token === undefined) return
             this.pageID = currentPageID
-            sendPing(token.get("left"),token.get("top"),token.get("_pageid"),"",true, pid);
+            sendPing(token.get("left"), token.get("top"), token.get("_pageid"), "", true, pid);
         });
     }
-    private onNewPage():void { setTimeout(() => { this.pingPlayers(); }, 1500);}
-    protected initialise():void {
+
+    private onNewPage(): void {
+        setTimeout(() => {
+            this.pingPlayers();
+        }, 1500);
+    }
+
+    protected initialise(): void {
         on('change:campaign:playerpageid', () => this.onNewPage());
     }
 }
